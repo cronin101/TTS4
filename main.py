763769758e51baf5eac562ef80412e4d6114cbb3
Aaron Cronin <cronin@include.cat>
2013-11-16 02:32:58 +0000

    Refactor

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from graph_reader import GraphReader
 from page_ranker  import PageRanker
 from hub_auth_ranker import HubAuthRanker
 
-page_ranker = PageRanker(GraphReader('./graph.txt').edges).iterate(10)
+N_ITER = 10
+N_RESULTS = 10
 
-hub_auth_ranker = HubAuthRanker(GraphReader('./graph.txt').edges).iterate(10)
+edgeset = GraphReader('./graph.txt').edges
+
+page_ranker = PageRanker(edgeset).iterate(N_ITER)
+
+hub_auth_ranker = HubAuthRanker(edgeset).iterate(N_ITER)
 
 def dump(ostream, ranks):
     ostream.write('\n'.join(' '.join(["%.8f" % s, e]) for (e, s) in ranks) + '\n')
 
-with open('./pr.txt', 'w') as p_r: dump(p_r, page_ranker.highest(10))
+with open('./pr.txt', 'w') as p_r: dump(p_r, page_ranker.highest(N_RESULTS))
 
-with open('./hub.txt', 'w') as hub_r: dump(hub_r, hub_auth_ranker.highest_hubs(10))
+with open('./hub.txt', 'w') as hub_r: dump(hub_r, hub_auth_ranker.highest_hubs(N_RESULTS))
 
-with open('./auth.txt', 'w') as auth_r: dump(auth_r, hub_auth_ranker.highest_auths(10))
+with open('./auth.txt', 'w') as auth_r: dump(auth_r, hub_auth_ranker.highest_auths(N_RESULTS))
