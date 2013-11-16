7d207db5ea5184948783761c71595fbec8d6d7ae
Aaron Cronin <cronin@include.cat>
2013-11-16 01:14:09 +0000

    pr.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graph_reader import GraphReader
 from page_ranker  import PageRanker
 
-ranker = PageRanker(GraphReader('./graph.txt').edges).iterate(9)
+ranker = PageRanker(GraphReader('./graph.txt').edges).iterate(10)
 
 with open('./pr.txt', 'w') as p_ranks:
     p_ranks.write('\n'.join(' '.join(["%.8f" % s, e]) for (e, s) in ranker.highest(10)) + '\n')
